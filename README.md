1bea21339ea283a6ae4ea9d686765eac23dfc7cf
Emeric <emeric.f@noze.tv>
2014-11-05 13:32:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# Cake3 Sluggable
+A simple Cake3 plugin to authenticate users with Cookies. This plugin is based on the awesome plugin [FriendsOfCake/Authenticate](https://github.com/FriendsOfCake/Authenticate/tree/cake3) but with a different setup.
+
+[![Build Status](https://img.shields.io/travis/Xety/Cake3-CookieAuth.svg?style=flat-square)](https://travis-ci.org/Xety/Cake3-CookieAuth)
+[![Coverage Status](https://img.shields.io/coveralls/Xety/Cake3-CookieAuth/master.svg?style=flat-square)](https://coveralls.io/r/xety/Cake3-CookieAuth)
+[![Scrutinizer](https://img.shields.io/scrutinizer/g/Xety/Cake3-CookieAuth.svg?style=flat-square)](https://scrutinizer-ci.com/g/Xety/Cake3-CookieAuth)
+[![Latest Stable Version](https://img.shields.io/packagist/v/Xety/Cake3-CookieAuth.svg?style=flat-square)](https://packagist.org/packages/xety/cake3-cookieauth)
+[![Total Downloads](https://img.shields.io/packagist/dt/xety/cake3-cookieauth.svg?style=flat-square)](https://packagist.org/packages/xety/cake3-cookieauth)
+[![License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](https://packagist.org/packages/xety/cake3-cookieauth)
+
+## Requirements
+* CakePHP 3.X
+
+## Installation
+Run : `composer require xety/cake3-cookieauth:1.*`
+Or add it in your `composer.json`:
+``` php
+"require": {
+	"xety/cake3-cookieauth": "1.*"
+},
+```
+
+## Usage
+In your `config/bootstrap.php` add :
+``` php
+Plugin::load('Xety/Cake3CookieAuth');
+```
+
+In your `AppController` :
+``` php
+public $components = [
+	'Cookie',
+	'Auth' => [
+		'authenticate' => [
+			'Form',
+			'Xety/Cake3CookieAuth.Cookie'
+		]
+	]
+			
+];
+```
+
+In your `AppController`, in the `beforeFilter` action :
+``` php
+public function beforeFilter(Event $event) {
+	//Automaticaly Login.
+	if (!$this->Auth->user() && $this->Cookie->read('CookieAuth')) {
+
+		$user = $this->Auth->identify();
+		if ($user) {
+			$this->Auth->setUser($user);
+		} else {
+			$this->Cookie->delete('CookieAuth');
+		}
+	}
+}
+
+//If you want to update some fields, like the last_login_date, or last_login_ip, just do :
+public function beforeFilter(Event $event) {
+	//Automaticaly Login.
+	if (!$this->Auth->user() && $this->Cookie->read('CookieAuth')) {
+		$this->loadModel('Users');
+
+		$user = $this->Auth->identify();
+		if ($user) {
+			$this->Auth->setUser($user);
+
+			$user = $this->Users->newEntity($user);
+			$user->isNew(false);
+			
+			//Last login date
+			$user->last_login = new Time();
+			//Last login IP
+			$user->last_login_ip = $this->request->clientIp();
+			//etc...
+
+			$this->Users->save($user);
+		} else {
+			$this->Cookie->delete('CookieAuth');
+		}
+	}
+}
+```
+
+In your `login` action, after `$this->Auth->setUser($user);` :
+``` php
+//It will write Cookie without RememberMe checkbox
+$this->Cookie->configKey('CookieAuth', [
+	'expires' => '+1 year',
+	'httpOnly' => true
+]);
+$this->Cookie->write('CookieAuth', [
+	'username' => $this->request->data('username'),
+	'password' => $this->request->data('password')
+]);
+
+
+//If you want use a RememberMe checkbox in your form :
+//In your view
+echo $this->Form->checkbox('remember_me');
+
+//In the login action :
+if($this->request->data('remember_me')) {
+	$this->Cookie->configKey('CookieAuth', [
+		'expires' => '+1 year',
+		'httpOnly' => true
+	]);
+	$this->Cookie->write('CookieAuth', [
+		'username' => $this->request->data('username'),
+		'password' => $this->request->data('password')
+	]);
+}
+```
+
+## Contribute
+[Follow this guide to contribute](https://github.com/Xety/Cake3-CookieAuth/blob/master/CONTRIBUTING.md)
---
+++ tests/TestCase/Auth/CookieAuthenticateTest.php
@@ -0,0 +1,148 @@
+<?php
+namespace Xety\Cake3CookieAuth\Test\TestCase\Auth;
+
+use Cake\Controller\ComponentRegistry;
+use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\I18n\Time;
+use Cake\Network\Request;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+use Cake\Routing\Router;
+use Cake\TestSuite\TestCase;
+use Cake\Utility\Security;
+use Xety\Cake3CookieAuth\Auth\CookieAuthenticate;
+
+class CookieAuthenticateTest extends TestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = ['plugin.Xety\Cake3CookieAuth.users'];
+
+/**
+ * setup
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->request = new Request('posts/index');
+		Router::setRequestInfo($this->request);
+		$this->response = $this->getMock('Cake\Network\Response');
+
+		Security::salt('Xety-Cake3CookieAuth_Xety-Cake3CookieAuth');
+		$this->registry = new ComponentRegistry(new Controller($this->request, $this->response));
+		$this->registry->load('Cookie');
+		$this->registry->load('Session');
+		$this->registry->load('Auth');
+		$this->auth = new CookieAuthenticate($this->registry);
+
+		$password = password_hash('password', PASSWORD_DEFAULT);
+		$Users = TableRegistry::get('Users');
+		$Users->updateAll(['password' => $password], []);
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		$this->registry->Cookie->delete('CookieAuth');
+	}
+
+/**
+ * test authenticate
+ *
+ * @return void
+ */
+	public function testAuthenticate() {
+		$expected = array(
+			'id' => 1,
+			'username' => 'Mariano'
+		);
+
+		$result = $this->auth->authenticate($this->request, $this->response);
+		$this->assertFalse($result);
+
+		$this->registry->Cookie->write(
+			'CookieAuth',
+			['username' => 'Mariano', 'password' => 'password']
+		);
+		$result = $this->auth->authenticate($this->request, $this->response);
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * test authenticateNoUsername
+ *
+ * @return void
+ */
+	public function testAuthenticateNoUsername() {
+		$this->registry->Cookie->write(
+			'CookieAuth',
+			['username' => '', 'password' => 'password']
+		);
+
+		$result = $this->auth->authenticate($this->request, $this->response);
+		$this->assertFalse($result);
+	}
+
+/**
+ * test authenticateFail
+ *
+ * @return void
+ */
+	public function testAuthenticateFail() {
+		$this->registry->Cookie->write(
+			'CookieAuth',
+			['username' => 'Mariano', 'password' => 'passwordfail']
+		);
+		$result = $this->auth->authenticate($this->request, $this->response);
+		$this->assertFalse($result);
+	}
+
+/**
+ * test authenticateNoCookieComponent
+ *
+ * @return void
+ */
+	public function testAuthenticateNoCookieComponent() {
+		$this->_registry = new ComponentRegistry(new Controller($this->request, $this->response));
+		$this->_registry->load('Cookie');
+		$this->_auth = new CookieAuthenticate($this->_registry);
+
+		$this->assertTrue($this->_registry->loaded('Cookie'));
+
+		$this->_registry->unload('Cookie');
+
+		$this->setExpectedException('RuntimeException');
+		$this->_auth->authenticate($this->request, $this->response);
+	}
+
+/**
+ * test logout
+ *
+ * @return void
+ */
+	public function testLogout() {
+		$this->registry->Cookie->write(
+			'CookieAuth',
+			['username' => 'Mariano', 'password' => 'password']
+		);
+		$user = $this->auth->authenticate($this->request, $this->response);
+
+		$resultTrue = $this->registry->Cookie->check('CookieAuth');
+		$this->assertTrue($resultTrue);
+
+		$this->auth->logout($user);
+		$resultFalse = $this->registry->Cookie->check('CookieAuth');
+		$this->assertFalse($resultFalse);
+	}
+
+}
